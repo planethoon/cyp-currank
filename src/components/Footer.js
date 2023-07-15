829ce1e8eea318ed69b03025791ad7d077ad6b42
planethoon <dev.sunghoon@gmail.com>
2023-07-15 16:32:49 +0900

    Fix: footer link, open in new tab

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,12 +8,14 @@ export default function Footer({ children }) {
         <div className={"footer--email"}>
           요청&피드백 : dev.sunghoon@gmail.com
         </div>
-        <Link href="https://developers.neople.co.kr" target="_blank">
-          <div className={"footer--neople"}>
-            <span>Powered by </span>
-            <span>Neople </span>
-            <span>OpenAPI</span>
-          </div>
+        <Link href="https://developers.neople.co.kr">
+          <a target="_blank">
+            <div className={"footer--neople"}>
+              <span>Powered by </span>
+              <span>Neople </span>
+              <span>OpenAPI</span>
+            </div>
+          </a>
         </Link>
       </div>
     </div>
