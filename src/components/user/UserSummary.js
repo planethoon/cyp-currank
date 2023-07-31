b249f5101fed25b026d554e9a6fb931fdb77fe15
planethoon <dev.sunghoon@gmail.com>
2023-07-31 20:43:06 +0900

    Add: summary rank skeleton

--- src/components/user/UserSummary.js
+++ src/components/user/UserSummary.js
@@ -1,5 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import UserSummaryRank from "./UserSummaryRank";
 
 export default function UserSummary() {
-  return <div className="user--summary"></div>;
+  const userInfo = {
+    playerId: "",
+    nickname: "Solidity",
+    tierName: "BRONZE 4TH",
+    ratingPoint: 2400,
+    maxRatingPoint: 2500,
+    rank: 0,
+    wins: 40,
+    loses: 20,
+    totalGames: 60,
+  };
+
+  return (
+    <div className="user--summary">
+      <UserSummaryRank userInfo={userInfo} />
+    </div>
+  );
 }
