1a42b63fc6230a85ed2d6b3aca460392d41e32da
planethoon <dev.sunghoon@gmail.com>
2023-08-11 20:18:43 +0900

    Fix: use match query hook.

--- src/components/user/UserMatch.jsx
+++ src/components/user/UserMatch.jsx
@@ -1,12 +1,30 @@
 import React from "react";
 import Match from "./match/Match";
-import useRatingMatchesQuery from "../../react-query/useMatchesQuery";
+import useMatchesQuery from "../../react-query/useMatchesQuery";
 import { useRouter } from "next/router";
+
+// export default function UserMatch() {
+//   const router = useRouter();
+//   const { nickname } = router.query;
+//   const { data, isLoading } = useMatchesQuery(nickname);
+
+//   if (isLoading) {
+//     return <div>로딩중</div>;
+//   }
+
+//   return (
+//     <div className="user--match--container">
+//       {data.matches.map((e) => {
+//         return <Match key={e.matchId} matchInfo={e} />;
+//       })}
+//     </div>
+//   );
+// }
 
 export default function UserMatch() {
   const router = useRouter();
   const { nickname } = router.query;
-  const { data, isLoading } = useRatingMatchesQuery(nickname);
+  const { data, isLoading } = useMatchesQuery(nickname);
 
   if (isLoading) {
     return <div>로딩중</div>;
--- src/react-query/useMatchesQuery.js
+++ src/react-query/useMatchesQuery.js
@@ -1,14 +1,15 @@
 import { useQuery } from "react-query";
 
-function useRatingMatchesQuery(nickname) {
-  return useQuery({
+function useMatchesQuery(nickname, gameType = "rating") {
+  const config = {
     queryKey: ["ratingMatches", nickname],
     queryFn: async () => {
-      let res = await fetch(`/api/getMatches/${nickname}?gameType=rating`);
+      let res = await fetch(`/api/getMatches/${nickname}?gameType=${gameType}`);
       let json = res.json();
       return json;
     },
-  });
+  };
+  return useQuery(config);
 }
 
-export default useRatingMatchesQuery;
+export default useMatchesQuery;
