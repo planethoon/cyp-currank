bee4842644932bb5e661dbbdfb028f667b0499ed
planethoon <dev.sunghoon@gmail.com>
2023-08-17 13:54:41 +0900

    Fix: match to relate to gametype

--- src/components/user/UserMatch.jsx
+++ src/components/user/UserMatch.jsx
@@ -23,19 +23,23 @@ import { useInView } from "react-intersection-observer";
 //   );
 // }
 
-export default function UserMatch() {
+export default function UserMatch({ gameType }) {
   const [targetRef, inView] = useInView({ rootMargin: "300px" });
   const router = useRouter();
   const { nickname } = router.query;
 
-  const { data, fetchNextPage, isLoading, hasNextPage } =
-    useMatchesInfiniteQuery(nickname, "rating");
+  const { data, fetchNextPage, isLoading, hasNextPage, refetch } =
+    useMatchesInfiniteQuery(nickname, gameType);
 
   useEffect(() => {
     if (hasNextPage && inView) {
       fetchNextPage();
     }
   }, [inView, hasNextPage]);
+
+  useEffect(() => {
+    refetch();
+  }, [gameType]);
 
   if (isLoading) {
     return <div>로딩중</div>;
