3e86185e8c823cdfb8fc8ca39ef854fe8a6f4828
planethoon <dev.sunghoon@gmail.com>
2023-10-27 19:55:13 +0900

    Fix: recent localstorage error fixed

--- src/components/Recent.jsx
+++ src/components/Recent.jsx
@@ -9,27 +9,31 @@ function Recent() {
 
   useEffect(() => {
     const local = localStorage.getItem("cypgg");
-    setHistory(JSON.parse(local));
+    if (local) {
+      setHistory(JSON.parse(local));
+    }
   }, []);
 
   return (
     <div className="recent--container">
       <div className="recent--title">최근 검색</div>
-      {history.map((nickname) => {
-        return (
-          <div
-            key={`${nickname}list`}
-            className="recent--list"
-            onClick={() => {
-              // eslint-disable-next-line react-hooks/rules-of-hooks
-              useLocalStorage(nickname);
-              router.push(`/user/${nickname}`);
-            }}
-          >
-            {nickname}
-          </div>
-        );
-      })}
+      <div className="recent--listWrapper">
+        {history.map((nickname) => {
+          return (
+            <div
+              key={`${nickname}list`}
+              className="recent--list"
+              onClick={() => {
+                // eslint-disable-next-line react-hooks/rules-of-hooks
+                useLocalStorage(nickname);
+                router.push(`/user/${nickname}`);
+              }}
+            >
+              {nickname}
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
